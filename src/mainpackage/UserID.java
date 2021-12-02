205f0f649637499855add9b96355673f6b908606
Lrc <mister_john_973@hotmail.com>
2021-12-02 12:30:24 +0100

    Revert "UserID fait"
    This reverts commit ab548160ea1ffdb911f557f4d4eab5eca23deb0e.

--- src/mainpackage/UserID.java
+++ src/mainpackage/UserID.java
@@ -1,45 +1,5 @@
 package mainpackage;
 
 public class UserID {
-	    // Déclaration des strings depuis cette classe seulement
-	    private String nom;
-	    private String hostAddress;
-	    
-	    // Definition de nom et de @IP
-	    protected void User(String nom, String address) {
-	        this.nom=nom;
-	        this.hostAddress=address;
-	    }
-	    
-	    // Lire le nom dans le string GetNom
-	    public String getNom() {
-	    	return this.nom;
-	    	}
-	    // Ecrire le nom dans le string name
-	    public String setNom(String name) {
-	    	return nom = name;
-	    	}
-	    
-	    // Lire l'@IP de l'utilisateur
-	    public String getAddress() { 
-	    	return this.hostAddress;  // hostAddress prend l'@IP
-	    	}
-	    // Afficher le nom et l'adresse IP de l'utilisateur
-	    public String toString() {
-	    	return this.getNom()+" : "+this.getAddress();
-	    	}
-	    
-	    public boolean equals(Object o)
-	    {
-	    	// Si l'utilisateur ne rentre pas de login
-	        UserID u = null;
-	        if (o != null && o instanceof UserID){
-	            u = (UserID) o;
-	        }
-	        // Si l'utilisateur rentre un pseudo
-	        if(u != null)
-	            return (this.hostAddress.equals(u.getAddress()));
-	        else
-	            return false;
-	    }
-	}
+
+}
