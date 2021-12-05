3d0fc75fb6a59ec1f30882405786509e660cc3d1
scanim-valentin <valentin.guerbette@hotmail.fr>
2021-12-05 23:39:08 +0100

    Chatting Session extends Thread

--- src/ChattingSession.java
+++ src/ChattingSession.java
@@ -1,3 +1,19 @@
-public class ChattingSession {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ChattingSession extends Thread {
+	
+	private UserID other_user ; //Other participant to the conversation
+	
+	private List<Message> message_list = new ArrayList<Message>() ;  //List of messages (history)
+	
+	public ChattingSession(UserID other_user, String threadname) {
+		super(threadname);
+		this.other_user = other_user ; 
+	}
+	
+	static private void debugPrint(String str) {
+		System.out.println("["+Thread.currentThread().getName()+"] ChattingSession: "+str);
+	}
+	
 }
