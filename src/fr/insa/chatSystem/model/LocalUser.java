25a58df28b80414342cc053c844cdc3928e2ce4c
Lrc354 <mister_john_973@hotmail.com>
2021-12-15 12:56:32 +0100

    modif

---
+++ src/fr/insa/chatSystem/model/LocalUser.java
@@ -0,0 +1,9 @@
+package fr.insa.chatSystem.model;
+
+public class LocalUser extends UserID{
+ 
+	public LocalUser(String name, String hostAddress){
+        super(name, hostAddress);
+    }
+    
+}
