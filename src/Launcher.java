14ed77beb3bc126b9e5733a2d8f76d9d414d04c2
scanim-valentin <valentin.guerbette@hotmail.fr>
2021-12-15 11:00:11 +0100

    tcp

--- src/Launcher.java
+++ src/Launcher.java
@@ -6,8 +6,11 @@ public class Launcher {
 		public static String pseudo;
 
 		public static void main(String[] args){
-	
-				// Ouvrir la fenetre de connexion
-				new MainWindow(pseudo);
+                if(args.length == 0){
+                    // Ouvrir la fenetre de connexion
+                    new MainWindow(pseudo);
+                }else{
+                    MainClass.nogui();
+                }
 			}
 	    }
